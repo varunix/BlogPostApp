9521d341de0755e5130199cb33ccd224f5475924
Varun Chaubey <varunchaubey4@gmail.com>
2022-11-02 23:04:05 +0530

    Updated About Section

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 export const About = () => {
+  let aboutHeading = {
+    textAlign: "center",
+    fontSize: "50px",
+    fontFamily: "Fantasy"
+  }
+
   return (
-    <div className="container">
-      This is an about component
+    <div className="container" style={aboutHeading}>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eius
-        ducimus aut veniam qui? Architecto culpa odit, nam molestiae quaerat
-        porro itaque nobis debitis explicabo maxime ad, natus corporis facilis.
+        Welcome! I am Varun Chaubey, I have created this blog website using ReactJs where you can create, edit, like or delete blogs. You can use this website to write your own thoughts about anything. If you like something then you can also like the blog using like button and more people can get appreciation for their work. Happy Writing!!
       </p>
     </div>
   );
